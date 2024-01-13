1131c04f20a00639ecbbe511df97cf3e1085901a
SHUBHAM GYAWALI <107835523+gyawalishubham07@users.noreply.github.com>
2024-01-14 02:07:37 +0545

    colorchanger.jsx

--- src/components/colorchanger.jsx
+++ src/components/colorchanger.jsx
@@ -18,7 +18,7 @@ function Example() {
         style={{ backgroundColor: changecolor }} >
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-            <input type="text"
+            <input type="text" placeholder="Enter color code/name"
         value={inputcolor}
         onChange={(e) => setInputcolor(e.target.value)}/>
             <button onClick={() => setchangecolor(handleCopy)} className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
